f20572a62b881175840585c56971edb5f9e2c76c
Darcy Laycock <sutto@sutto.net>
2010-07-29 19:09:45 +0800

    Bump version

--- lib/bhm/admin.rb
+++ lib/bhm/admin.rb
@@ -3,7 +3,7 @@ require 'active_support'
 module BHM
   module Admin
     
-    VERSION = "0.2.0".freeze
+    VERSION = "0.2.1".freeze
     
     # Helpers for use in the admin area.
     autoload :SidebarHelper,         'bhm/admin/sidebar_helper'
